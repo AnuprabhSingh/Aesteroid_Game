590c2e240e1d0951660e1761f5f199b5e05c66dc
Anuprabh Singh <anuprabh.2004@gmail.com>
2024-11-08 02:04:35 +0530

    multiple files + hints

---
+++ sprites.py
@@ -0,0 +1,116 @@
+import arcade
+import math
+from constants import *
+
+# Constants to avoid cyclic imports
+
+
+class TurningSprite(arcade.Sprite):
+    """ Sprite that sets its angle to the direction it is traveling in. """
+    def update(self):
+        """ Move the sprite """
+        super().update()
+        self.angle = math.degrees(math.atan2(self.change_y, self.change_x))
+
+
+class ShipSprite(arcade.Sprite):
+    """
+    Sprite that represents our space ship.
+
+    Derives from arcade.Sprite.
+    """
+    def __init__(self, filename, scale):
+        """ Set up the space ship. """
+
+        # Call the parent Sprite constructor
+        super().__init__(filename, scale)
+
+        # Info on where we are going.
+        # Angle comes in automatically from the parent class.
+        self.thrust = 0
+        self.speed = 0
+        self.max_speed = 4
+        self.drag = 0.05
+        self.respawning = 0
+
+        # Mark that we are respawning.
+        self.respawn()
+
+    def respawn(self):
+        """
+        Called when we die and need to make a new ship.
+        'respawning' is an invulnerability timer.
+        """
+        # If we are in the middle of respawning, this is non-zero.
+        self.respawning = 1
+        self.center_x = SCREEN_WIDTH / 2
+        self.center_y = SCREEN_HEIGHT / 2
+        self.angle = 0
+
+    def update(self):
+        """
+        Update our position and other particulars.
+        """
+        if self.respawning:
+            self.respawning += 1
+            self.alpha = self.respawning
+            if self.respawning > 250:
+                self.respawning = 0
+                self.alpha = 255
+        if self.speed > 0:
+            self.speed -= self.drag
+            if self.speed < 0:
+                self.speed = 0
+
+        if self.speed < 0:
+            self.speed += self.drag
+            if self.speed > 0:
+                self.speed = 0
+
+        self.speed += self.thrust
+        if self.speed > self.max_speed:
+            self.speed = self.max_speed
+        if self.speed < -self.max_speed:
+            self.speed = -self.max_speed
+
+        self.change_x = -math.sin(math.radians(self.angle)) * self.speed
+        self.change_y = math.cos(math.radians(self.angle)) * self.speed
+
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
+        # If the ship goes off-screen, move it to the other side of the window
+        if self.right < 0:
+            self.left = SCREEN_WIDTH
+
+        if self.left > SCREEN_WIDTH:
+            self.right = 0
+
+        if self.bottom < 0:
+            self.top = SCREEN_HEIGHT
+
+        if self.top > SCREEN_HEIGHT:
+            self.bottom = 0
+
+        """ Call the parent class. """
+        super().update()
+
+
+class AsteroidSprite(arcade.Sprite):
+    """ Sprite that represents an asteroid. """
+
+    def __init__(self, image_file_name, scale):
+        super().__init__(image_file_name, scale=scale)
+        self.size = 0
+
+    def update(self):
+        """ Move the asteroid around. """
+        super().update()
+        if self.center_x < LEFT_LIMIT:
+            self.center_x = RIGHT_LIMIT
+        if self.center_x > RIGHT_LIMIT:
+            self.center_x = LEFT_LIMIT
+        if self.center_y > TOP_LIMIT:
+            self.center_y = BOTTOM_LIMIT
+        if self.center_y < BOTTOM_LIMIT:
+            self.center_y = TOP_LIMIT
